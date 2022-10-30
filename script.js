19944596567d8bdcd38a984faaba5947be554ef3
ps8847 <ps789650@gmail.com>
2022-10-30 17:06:51 +0530

    made changes for better speed

--- script.js
+++ script.js
@@ -1,16 +1,15 @@
 $(document).ready(function() {
     $(window).scroll(function() {
-        if (this.scrollY > 20) {
-            $('.navbar').addClass("sticky");
-        } else {
-            $('.navbar').removeClass("sticky");
-
-        }
-        if (this.scrollY > 500) {
-            $('.scroll-up-btn').addClass("show");
-        } else {
-            $('.scroll-up-btn').removeClass("show");
-        }
+      if (this.scrollY > 20) {
+        $(".navbar").addClass("sticky");
+      } else {
+        $(".navbar").removeClass("sticky");
+      }
+      if (this.scrollY > 500) {
+        $(".scroll-up-btn").addClass("show");
+      } else {
+        $(".scroll-up-btn").removeClass("show");
+      }
     });
 
     // scroll up btn 
