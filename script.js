80db69a6b11a68f1cd69b8a88f55fa1ad344b120
ps8847 <ps789650@gmail.com>
2022-11-11 10:51:19 +0530

    added worksample

--- script.js
+++ script.js
@@ -1,67 +1,67 @@
 $(document).ready(function() {
-    $(window).scroll(function() {
-      if (this.scrollY > 20) {
-        $(".navbar").addClass("sticky");
-      } else {
-        $(".navbar").removeClass("sticky");
-      }
-      if (this.scrollY > 500) {
-        $(".scroll-up-btn").addClass("show");
-      } else {
-        $(".scroll-up-btn").removeClass("show");
-      }
-    });
-
-    // scroll up btn 
-    $('.scroll-up-btn').click(function() {
-        $('html').animate({ scrollTop: 0 });
-    });
+  $(window).scroll(function () {
+    if (this.scrollY > 20) {
+      $(".navbar").addClass("sticky");
+    } else {
+      $(".navbar").removeClass("sticky");
+    }
+    if (this.scrollY > 500) {
+      $(".scroll-up-btn").addClass("show");
+    } else {
+      $(".scroll-up-btn").removeClass("show");
+    }
+  });
 
 
-    // tyoing animation
+  // scroll up btn
+  $(".scroll-up-btn").click(function () {
+    $("html").animate({ scrollTop: 0 });
+  });
 
-    var typed = new Typed(".typing", {
-        strings: ["Web Designer", "Web Developer", "Freelancer", "Blogger"],
-        typeSpeed: 100,
-        backSpeed: 60,
-        loop: true
-    })
+  // tyoing animation
 
-    var typed = new Typed(".typing2", {
-        strings: ["Web Designer", "Web Developer", "Freelancer", "Blogger"],
-        typeSpeed: 100,
-        backSpeed: 60,
-        loop: true
-    })
+  var typed = new Typed(".typing", {
+    strings: ["Web Designer", "Web Developer", "Freelancer", "Blogger"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true,
+  });
 
-    // toggle menu navbar script
-    $('.menu-btn').click(function() {
-        $('.navbar .menu').toggleClass("active");
-        $('.menu-btn i').toggleClass("active");
-    })
+  var typed = new Typed(".typing2", {
+    strings: ["Web Designer", "Web Developer", "Freelancer", "Blogger"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true,
+  });
 
-    // owl crousel script
+  // toggle menu navbar script
+  $(".menu-btn").click(function () {
+    $(".navbar .menu").toggleClass("active");
+    $(".menu-btn i").toggleClass("active");
+  });
 
-    $('.carousel').owlCarousel({
-        margin: 20,
-        loop: true,
-        autoplayTimeOut: 2000,
-        autoplayHoverPause: true,
-        responsive: {
-            0: {
-                items: 1,
-                nav: false,
-            },
-            600: {
-                items: 2,
-                nav: false,
-            },
-            1000: {
-                items: 3,
-                nav: false,
-            }
-        }
-    })
+  // owl crousel script
+
+  $(".carousel").owlCarousel({
+    margin: 20,
+    loop: true,
+    autoplayTimeOut: 2000,
+    autoplayHoverPause: true,
+    responsive: {
+      0: {
+        items: 1,
+        nav: false,
+      },
+      600: {
+        items: 2,
+        nav: false,
+      },
+      1000: {
+        items: 3,
+        nav: false,
+      },
+    },
+  });
 });
 
 
